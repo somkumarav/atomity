b3a4a2fea2388a5bff0a22de6310605e5b15f7ad
somkumarav <somkumarav@gmail.com>
2026-03-18 21:25:03 +0000

    feat: add server to provide with necessary data

---
+++ actions/cluster.action.ts
@@ -0,0 +1,89 @@
+export const getClusterInfo = () => {
+  return [
+    {
+      name: "Cluster A",
+      cpu: 2463,
+      ram: 1368,
+      storage: 246,
+      network: 307,
+      gpu: 821,
+      efficiency: 10,
+      total: 6867,
+    },
+    {
+      name: "Cluster B",
+      cpu: 2127,
+      ram: 1181,
+      storage: 212,
+      network: 265,
+      gpu: 0,
+      efficiency: 28,
+      total: 5574,
+    },
+    {
+      name: "Cluster C",
+      cpu: 1733,
+      ram: 962,
+      storage: 173,
+      network: 216,
+      gpu: 577,
+      efficiency: 15,
+      total: 4664,
+    },
+    {
+      name: "Cluster D",
+      cpu: 1218,
+      ram: 677,
+      storage: 121,
+      network: 152,
+      gpu: 0,
+      efficiency: 50,
+      total: 2545,
+    },
+  ];
+};
+
+export const getNameSpaceInfo = () => {
+  return [
+    {
+      name: "Namespace A",
+      cpu: 1231,
+      ram: 684,
+      storage: 123,
+      network: 153,
+      gpu: 410,
+      efficiency: 5,
+      total: 3433,
+    },
+    {
+      name: "Namespace B",
+      cpu: 739,
+      ram: 410,
+      storage: 73,
+      network: 92,
+      gpu: 246,
+      efficiency: 20,
+      total: 2060,
+    },
+    {
+      name: "Namespace C",
+      cpu: 369,
+      ram: 205,
+      storage: 36,
+      network: 46,
+      gpu: 123,
+      efficiency: 50,
+      total: 1030,
+    },
+    {
+      name: "Namespace D",
+      cpu: 123,
+      ram: 68,
+      storage: 12,
+      network: 15,
+      gpu: 41,
+      efficiency: 40,
+      total: 343,
+    },
+  ];
+};
